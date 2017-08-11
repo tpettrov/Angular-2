6e24ef11f203671b9b6c74de369041b9080d053a
tpettrov <tpettrov@gmail.com>
2017-08-11 20:53:46 +0300

    stats working

--- workshop/Client/src/app/stats/stats.component.ts
+++ workshop/Client/src/app/stats/stats.component.ts
@@ -8,12 +8,17 @@ import {StatsService} from "./stats.service";
 })
 
 export class StatsComponent implements OnInit {
-   cars: 0;
-   users: 0;
+  cars: 0;
+  users: 0;
 
-   constructor(private statService: StatsService) {}
+  constructor(private statService: StatsService) {
+  }
+
   ngOnInit(): void {
-     this.statService.getStats().subscribe((stats) => console.log(stats));
+    this.statService.getStats().subscribe((stats) => {
+      this.cars = stats.cars;
+      this.users = stats.users;
+    });
   }
 
 }
